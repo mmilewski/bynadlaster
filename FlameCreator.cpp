394d08ec8ecfc35464c92bf7e57d8093fd0206d9
Marcin Milewski <mmilewski@gmail.com>
2009-09-22 18:39:09 +0000

    - poprawiłem komentarze w FlameCreator.cpp

--- FlameCreator.cpp
+++ FlameCreator.cpp
@@ -11,9 +11,8 @@ FlameCreator::FlameCreator(size_t owner_id, Position initial_position, size_t ra
 
 void FlameCreator::Create(Game& game) {
   MapPtr map = game.GetMap();
-  const Position pos = m_position;
-  double up(0), down(0), left(0), right(0);  // length of flame in four directions
-  double length_up(0), length_left(0), length_down(0), length_right(0);
+  const Position pos = m_position;   // center (left,bottom point) of the fire/flame/blast/whatever
+  double length_up(0), length_left(0), length_down(0), length_right(0);  // length of flame in four directions
 
   // determine how long can flame become because of map obstacles
   for (size_t i=1; i<=GetRange(); ++i) {
@@ -41,16 +40,17 @@ void FlameCreator::Create(Game& game) {
       break;
   }
 
+  // we need to rescale width & height of flames' lengths
   const double flameWidth = 1.0/g_tiles_on_screen_in_x;
   const double flameHeight = 1.0/g_tiles_on_screen_in_y;
-  up = length_up/static_cast<double>(g_tiles_on_screen_in_y);
-  down = length_down/static_cast<double>(g_tiles_on_screen_in_y);
-  left = length_left/static_cast<double>(g_tiles_on_screen_in_x);
-  right = length_right/static_cast<double>(g_tiles_on_screen_in_x);
+  const double up = length_up/static_cast<double>(g_tiles_on_screen_in_y);
+  const double down = length_down/static_cast<double>(g_tiles_on_screen_in_y);
+  const double left = length_left/static_cast<double>(g_tiles_on_screen_in_x);
+  const double right = length_right/static_cast<double>(g_tiles_on_screen_in_x);
 
-  game.AddObject(FlamePtr(new Flame(pos+Position(1,0), Size(right,flameHeight), FD::Right)));
-  game.AddObject(FlamePtr(new Flame(pos+Position(0,1), Size(flameWidth,up), FD::Up)));
-  game.AddObject(FlamePtr(new Flame(pos-Position(length_left,0), Size(left,flameHeight), FD::Left)));
-  game.AddObject(FlamePtr(new Flame(pos-Position(0,length_down), Size(flameWidth,down), FD::Down)));
+  game.AddObject(FlamePtr(new Flame(pos+Position(1,0), Size(right,flameHeight), FD::Right)));         // right arm
+  game.AddObject(FlamePtr(new Flame(pos+Position(0,1), Size(flameWidth,up), FD::Up)));                // top arm
+  game.AddObject(FlamePtr(new Flame(pos-Position(length_left,0), Size(left,flameHeight), FD::Left)));  // left arm
+  game.AddObject(FlamePtr(new Flame(pos-Position(0,length_down), Size(flameWidth,down), FD::Down)));   // bottom arm
   game.AddObject(FlamePtr(new Flame(pos, Size(flameWidth,flameHeight), FD::Center)));
 }
